7f714e145593f12e097af80cae8c55d268b41bc1
Nandha Kumar <pujaa1201@gmail.com>
2024-04-23 16:51:30 +0530

    Added cache interceptor to provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from  '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from  '@angular/common/http';
 import { SharedModule } from './components/shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsModule } from './components/icons/icons.module';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
+import { CacheInterceptor } from './interceptors/cache-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,13 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CacheInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
